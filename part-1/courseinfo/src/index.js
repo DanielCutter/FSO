5a57d5e0551e8be7073c6dab5054f4586844b265
Daniel Cutter <dan.cutter@hotmail.co.uk>
2020-08-03 17:13:23 +0100

    ex. 1.5 completion - Single JSO

--- part-1/courseinfo/src/index.js
+++ part-1/courseinfo/src/index.js
@@ -3,43 +3,45 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
 const Header = (props) => {
   return (
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
       <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
+        <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+        <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+        <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
       </div>
   )
 }
@@ -47,7 +49,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
     <div>
-      <p><strong>{props.part.name}:</strong> {props.part.exercises} </p>
+      <p><strong>{props.name}:</strong> {props.exercises} </p>
     </div>
   )
 }
